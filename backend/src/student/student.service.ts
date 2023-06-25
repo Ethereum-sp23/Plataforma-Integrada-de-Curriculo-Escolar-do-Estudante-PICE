2d54c3cee78e828c7f26752ca5d24bd5315a5b63
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-06-25 03:42:32 -0300

    fix(backend): contract abi updated

--- backend/src/student/student.service.ts
+++ backend/src/student/student.service.ts
@@ -8,35 +8,39 @@ import { ResponseGetStudents } from './dto/student.dto';
 @Injectable()
 export class StudentService {
   async getAllNfts({ address }): Promise<any> {
-
     // pegar o array de todos os IDs dos NFTs de um usuario com base no endereço do usuario
-    console.log("conectando ao contrato no endereço", contractAddress)
-    const contract = new DappKitFunctions("abi???",'address???',);
+    const contract = new DappKitFunctions();
 
-    console.log("fazendo transação para pegar todos os IDs dos NFTs do usuario ", address)
-    const allIDs = await contract.adminGetTransaction("seeOwnedNFTs", [address])
-    console.log("IDS: ", allIDs)
+    console.log(
+      'fazendo transação para pegar todos os IDs dos NFTs do usuario ',
+      address,
+    );
+    const allIDs = await contract.adminGetTransaction('seeOwnedNFTs', [
+      address,
+    ]);
+    console.log('IDS: ', allIDs);
 
-    const allIPFSLinks = []
+    const allIPFSLinks = [];
     for (let item in allIDs) {
       // para cada ID, pegar o link do IPFS
-      console.log("adicionando o link do IPFS do NFT de ID ", item)
-      const retrievedIPFSLink = await contract.adminGetTransaction('getIPFSByID', [item]);
+      console.log('adicionando o link do IPFS do NFT de ID ', item);
+      const retrievedIPFSLink = await contract.adminGetTransaction(
+        'getIPFSByID',
+        [item],
+      );
 
       allIPFSLinks.push(retrievedIPFSLink);
-
     }
 
-    let allNftsResponse = []
+    let allNftsResponse = [];
 
     for (let link in allIPFSLinks) {
-      console.log("pegando dados do link ", link)
+      console.log('pegando dados do link ', link);
       const res = await axios.get(link);
       allNftsResponse.push(res.data);
     }
 
     return allNftsResponse;
-
   }
 
   async getStudentByName(name: string): Promise<string | ResponseGetStudents> {
