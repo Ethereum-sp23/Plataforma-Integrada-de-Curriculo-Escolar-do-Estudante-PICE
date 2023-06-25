a9e6cf41e28f2663e63c08bba2ba9158d105c43d
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-06-25 00:49:24 -0300

    feat(backend):dappKit class created

--- backend/src/government/gov.controller.ts
+++ backend/src/government/gov.controller.ts
@@ -30,6 +30,23 @@ export interface CreateSchoolBody {
   email: string;
   password: string;
 }
+
+interface Response {
+  message: string;
+  data?: any;
+}
+
+export interface CreatePersonBody {
+  name: string;
+  email: string;
+  course: string;
+}
+
+export interface CreateSchoolBody {
+  name: string;
+  email: string;
+  password: string;
+}
 @Controller()
 export class GovernmentController {
   constructor(private readonly GovernmentService: GovernmentService) {}
