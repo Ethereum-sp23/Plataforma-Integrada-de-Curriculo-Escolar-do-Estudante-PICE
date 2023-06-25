722335a025f1787bf6e3b888490608d97a5d519c
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-06-25 05:10:11 -0300

    Landing page responsiva

--- frontend/components/homeSection/index.tsx
+++ frontend/components/homeSection/index.tsx
@@ -14,11 +14,13 @@ const HomeSection = () => {
 
     return (
         <LandingContainer>
-            <div className="absolute bg-white w-1 h-[55%] rounded left-10 top-[30%]"></div>
-            <div className="absolute top-[30%] left-20 flex flex-col gap-4 items-start text-white">
-                <h1 className="text-9xl font-bold">PICE</h1>
-                <h3 className="uppercase text-2xl">Plataforma integrada de currículo escolar do estudante</h3>
-                <p className="w-[50%] text-lg mb-2">
+            <div className="absolute bg-white w-[2px] h-[40%] md:h-[40%] lg:h-[55%] rounded left-5 sm:left-10 top-[30%]"></div>
+            <div className="absolute top-[30%] left-10 sm:left-20 flex flex-col gap-4 items-start text-white">
+                <h1 className="text-7xl lg:text-9xl font-bold">PICE</h1>
+                <h3 className="uppercase text-xl lg:text-2xl">
+                    Plataforma integrada de currículo escolar do estudante
+                </h3>
+                <p className="w-[90%] sm:w-[50%] lg:w-[50%] text-sm lg:text-lg mb-2">
                     A PICE é uma plataforma de registro acadêmico baseada em blockchain que capacita os estudantes,
                     permitindo que eles destaquem suas conquistas de forma tangível em candidaturas e contribuindo para
                     uma educação de qualidade.
@@ -32,14 +34,14 @@ const HomeSection = () => {
                 </Link>
             </div>
 
-            <div className="absolute bottom-[5%] left-[75%] text-white text-2xl">
-                <div className="absolute w-[150%] top-[50%] translate-x-[-110%] translate-y-[-50%] bg-white h-1 rounded"></div>
+            <div className="absolute bottom-[5%] left-10 lg:left-[60%] xl:left-[75%] text-white text-2xl">
+                <div className="absolute w-[150%] top-[50%] -translate-x-[-1000%] xl:translate-x-[-110%] translate-y-[-50%] bg-white h-[2px] rounded"></div>
                 <p>Revolucione a</p>
                 <p className="uppercase font-bold text-5xl text-primary">Educação</p>
                 <p className="uppercase font-bold">Com a gente</p>
             </div>
 
-            <div className="absolute top-6 right-6">
+            <div className="absolute left-[50%] translate-x-[-50%] top-6 lg:left-auto lg:translate-x-0 lg:right-6">
                 <Button
                     ariaLabel="Institution Button"
                     className="button hover:scale-105 transition"
--- frontend/components/landingContainer/index.tsx
+++ frontend/components/landingContainer/index.tsx
@@ -7,9 +7,9 @@ interface LandingContainerProps {
 const LandingContainer = ({ children }: LandingContainerProps) => {
     return (
         <div className="h-[100vh] bg-landing bg-cover">
-            <div className="h-full w-full backdrop-blur-lg p-20">
-                <div className="bg-white h-full w-full rounded-2xl p-4">
-                    <div className="h-full w-full bg-landing bg-cover rounded-2xl shadow overflow-hidden ">
+            <div className="h-full w-full lg:backdrop-blur-lg lg:p-20">
+                <div className="lg:bg-white h-full w-full lg:rounded-2xl lg:p-4">
+                    <div className="h-full w-full bg-landing bg-cover lg:rounded-2xl shadow overflow-hidden ">
                         <div className="h-full w-full bg-opacity-60 bg-black relative">{children}</div>
                     </div>
                 </div>
